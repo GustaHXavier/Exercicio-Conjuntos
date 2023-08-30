383992d04e8f27d3c04c0ffc0e14b67a683ffc63
Gustavo Henrique <gugu2305@hotmail.com>
2023-08-30 19:07:27 -0300

    Exercicio Finalizado

---
+++ Exercicio_Hash/Entities/Aluno.cs
@@ -0,0 +1,21 @@
+﻿namespace Entities {
+    internal class Aluno {
+
+        public int Codigo { get; set; }
+
+        public Aluno(int codigo) {
+            Codigo = codigo;
+        }
+
+        public override int GetHashCode() {
+            return Codigo.GetHashCode();
+        }
+
+        public override bool Equals(object obj) {
+            if (!(obj is Aluno)) return false;
+
+            Aluno other = (Aluno)obj;   
+            return Codigo.Equals(other.Codigo);
+        }
+    }
+}
---
+++ Exercicio_Hash/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Entities;
+
+HashSet<Aluno> alunos = new HashSet<Aluno>();
+int num = 0;
+
+Console.Write("Quantos estudantes do curso A: ");
+int xA = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < xA; i++) {
+
+    num = int.Parse(Console.ReadLine());
+    alunos.Add(new Aluno(num));
+
+}
+
+Console.Write("Quantos estudantes do curso B: ");
+int xB = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < xB; i++) {
+
+    num = int.Parse(Console.ReadLine());
+    alunos.Add(new Aluno(num));
+
+}
+
+Console.Write("Quantos estudantes do curso C: ");
+int xC = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < xC; i++) {
+
+    num = int.Parse(Console.ReadLine());
+    alunos.Add(new Aluno(num));
+
+}
+
+Console.WriteLine("TOTAL DE ALUNOS: " + alunos.Count);
